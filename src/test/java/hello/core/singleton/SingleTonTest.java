f355c7510151cd093eaeb51b1a4e2fbef38c3573
jskim216 <gerad216@gmail.com>
2023-02-22 16:06:58 +0900

    스프링 핵심원리 기본편 - AppConfig 위치 수정 / 싱글톤 객체 TEST;

---
+++ src/test/java/hello/core/singleton/SingleTonTest.java
@@ -0,0 +1,45 @@
+package hello.core.singleton;
+
+import hello.core.member.MemberService;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingleTonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        AppConfig appConfig = new AppConfig();
+
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회 : 호출할 떄 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("MemberService1 = " + memberService1);
+        System.out.println("MemberService2 = " + memberService2);
+
+        // memberService1 !== memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singleTonServiceTest() {
+        SingleTonService singleTonService1 = SingleTonService.getInstance();
+        SingleTonService singleTonService2 = SingleTonService.getInstance();
+
+        System.out.println("singletonService1 = " + singleTonService1);
+        System.out.println("singletonService2 = " + singleTonService2);
+
+        assertThat(singleTonService1).isSameAs(singleTonService2);
+
+        // same == 객체 인스턴스 비교
+        // equal java equal 메소드
+    }
+}
