631d24ac88cd6739dc3a02f05adf07e06aa6e7e5
jskim216 <gerad216@gmail.com>
2023-02-24 10:15:32 +0900

    스프링 핵심원리 기본편 - 싱글톤 ~ 컴포넌트 스캔 및 자동주입

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>(); // 동시성 이슈가 있기에 ConcurrentHashMap 을 사용하여야 한다. 예제이니 넘어간다.
